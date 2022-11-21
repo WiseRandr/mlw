2ed42033d2675b7b3bf5645538e1641a9e48f855
Wise Randr <randriamialisonwise@gmail.com>
2022-11-21 21:17:59 +0300

    add task

--- src/pages/my-checklist-detail.page.tsx
+++ src/pages/my-checklist-detail.page.tsx
@@ -1,7 +1,8 @@
 import { useNavigation, useRoute } from "@react-navigation/core";
-import { useCallback, useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { When } from "react-if";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
+import uuid from 'react-native-uuid';
 import ChevronLeftSvg from "../icon/chevron-left-svg";
 import { useMyChecklist } from "../store";
 import { TaskType } from "../types/task.type";
@@ -11,32 +12,62 @@ import FONTS from "../ui/fonts";
 import UIText from "../ui/text/text";
 
 export default function MyChecklistDetailPage() {
+  // Hooks
   const route = useRoute();
   const navigation = useNavigation();
-  const id = useMemo(() => (route.params as any)?.id || '', [route]);
+
+  // Store
   const myChecklists = useMyChecklist(state => state.myChecklist);
-  const current = useMemo(() => myChecklists.find((c) => c.id === id), [id]);
+  const pushTask = useMyChecklist(state => state.pushTask);
+
+  // States
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [newTaskInput, setNewTaskInput] = useState<string>('');
+
+  // Memos
+  const id = useMemo(() => (route.params as any)?.id || '', [route]);
+  const current = useMemo(() => myChecklists.find((c) => c.id === id), [id, myChecklists]);
   const { todos, completed } = useMemo<{ todos: TaskType[], completed: TaskType[] }>(() => (current?.items || []).reduce((a, c) => {
     if (c.status === 'to-do') a.todos.push(c);
     if (c.status === 'completed') a.completed.push(c);
     return a;
   }, { todos: [] as TaskType[], completed: [] as TaskType[] }), [current]);
 
-  const goBack = useCallback(() => { navigation.goBack() }, []);
+  const clearEditing = useCallback(() => {
+    setIsEditing(false);
+    setNewTaskInput('');
+  }, []);
+  
+  const leftOnPress = useCallback(() => {
+    if (!isEditing) navigation.goBack();
+    else clearEditing();
+  }, [isEditing, clearEditing]);
+
+  const rightOnPress = useCallback(() => {
+    if (!isEditing) setIsEditing(true);
+    else {
+      if (newTaskInput && current) {
+        pushTask(current.id, { id: uuid.v4() as string, name: newTaskInput, status: 'to-do' });
+        clearEditing();
+      }
+    }
+  }, [newTaskInput, isEditing, current, pushTask, clearEditing]);
+
+  const handleChange = useCallback((text: string) => { setNewTaskInput(text); }, []);
 
   useEffect(() => {
     navigation.setOptions({
-      headerLeft: () => (<TouchableOpacity onPress={goBack} style={styles.headerLeft}><ChevronLeftSvg /><UIText>Lists</UIText></TouchableOpacity>),
-      headerRight: () => (<UIButton title="Edit List" />)
+      headerLeft: () => (<TouchableOpacity onPress={leftOnPress} style={styles.headerLeft}><ChevronLeftSvg /><UIText>{isEditing ? 'Cancel' : 'Lists'}</UIText></TouchableOpacity>),
+      headerRight: () => (<UIButton title={isEditing ? 'Save' : 'Edit List'} onPress={rightOnPress} />)
     });
-  }, []);
+  }, [isEditing, rightOnPress, leftOnPress]);
 
   return <View>
     <When condition={Boolean(current)}>
       <UIText style={styles.title}>{current?.name}</UIText>
 
       <View>
-        <When condition={todos.length > 0 && completed.length > 0}>
+        <When condition={todos.length > 0 && completed.length > 0 && !isEditing}>
           <UIText style={styles.menu}>To-dos</UIText>
         </When>
         {todos.map((todo) => (
@@ -45,14 +76,20 @@ export default function MyChecklistDetailPage() {
           </View>
         ))}
 
-        <When condition={todos.length > 0 && completed.length > 0}>
+        <When condition={isEditing}>
+          <TextInput style={styles.input} placeholder="Task" value={newTaskInput} onChangeText={handleChange} />
+        </When>
+
+        <When condition={todos.length > 0 && completed.length > 0 && !isEditing}>
           <UIText style={styles.menu}>Completed</UIText>
         </When>
-        {completed.map((comp) => (
-          <View key={comp.id} style={styles.item}>
-            <UIText style={[styles.itemText, styles.itemTextCompleted]}>{comp.name}</UIText>
-          </View>
-        ))}
+        <When condition={!isEditing}>
+          {completed.map((comp) => (
+            <View key={comp.id} style={styles.item}>
+              <UIText style={[styles.itemText, styles.itemTextCompleted]}>{comp.name}</UIText>
+            </View>
+          ))}
+        </When>
       </View>
     </When>
   </View>
@@ -86,5 +123,9 @@ const styles = StyleSheet.create({
   },
   itemTextCompleted: {
     color: COLORS.grey
+  },
+  input: {
+    paddingVertical: 20,
+    paddingHorizontal: 25,
   }
 });
--- src/store/useMyChecklist.ts
+++ src/store/useMyChecklist.ts
@@ -1,11 +1,13 @@
 import create from 'zustand';
 import { MyCheckListType } from '../types';
 import uuid from 'react-native-uuid';
+import { TaskType } from '../types/task.type';
 
 export const useMyChecklist = create<{
   myChecklist: MyCheckListType[],
   pushToChecklist: (input: MyCheckListType) => void,
   removeFromChecklist: (input: MyCheckListType) => void,
+  pushTask: (checklistId: string, task: TaskType) => void,
 }>((set) => ({
   myChecklist: [
     { id: uuid.v4(), name: 'Restaurants to visit in France', createdAt: new Date('2022-11-17'), items: [{ id: uuid.v4(), name: 'La Parfait', status: 'to-do' }, { id: uuid.v4(), name: 'Completed task', status: 'completed' }] },
@@ -17,5 +19,8 @@ export const useMyChecklist = create<{
   }),
   removeFromChecklist: (input: MyCheckListType) => set((state) => {
     return ({ ...state, myChecklist: state.myChecklist.filter((c) => c.id !== input.id) })
+  }),
+  pushTask: (checklistId: string, task: TaskType) => set((state) => {
+    return { ...state, myChecklist: state.myChecklist.map((ch) => ({ ...ch, items: ch.id === checklistId ? [...ch.items, task] : ch.items })) }
   })
 }));
